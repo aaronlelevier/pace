39f2a398835a761a8ebcc9e83e8500724f10f4cf
aaronlelevier <aaron.lelevier@gmail.com>
2023-08-22 19:49:33 -0700

    calculate effective top tube

--- pace/src/ping_handler.erl
+++ pace/src/ping_handler.erl
@@ -1,7 +1,13 @@
 %%%-------------------------------------------------------------------
 %% @doc ping/pong API
 %%
+%%  # HTTP
 %%  curl http://localhost:8080/ping
+%%
+%%  % validate effective top tube calculation
+%%  1> ping_handler:ett(ping_handler:sta({78.7, 600.0}, {78.1, 700.0}, 614.5), 614.5, 475).
+%%  598.7598468837922
+%%
 %%
 %% @end
 %%%-------------------------------------------------------------------
@@ -9,7 +15,14 @@
 -module(ping_handler).
 -behavior(cowboy_handler).
 
--export([init/2]).
+-export([
+    init/2,
+    radians/1,
+    sta/3,
+    effective_stl/2,
+    est_dist/2,
+    ett/3
+]).
 
 init(Req0, State) ->
     io:format("pace_handler request port: ~p.\n", ["8080"]),
@@ -18,3 +31,35 @@ init(Req0, State) ->
         <<"pong">>,
         Req0),
     {ok, Req, State}.
+
+
+% Returns the radians using the degrees 'N'
+radians(N) -> N * (math:pi()/180).
+
+
+% Returns the seat tube angle (STA) using:
+% Low = {STA, Height}
+% Height = {STA, Height}
+% Stack
+% ex: sta({78.7, 600.0}, {78.1, 700.0}, 614.5).
+sta(Low, High, Stack) ->
+    {SAL, SHL} = Low,
+    {SAH, SHH} = High,
+    SAL - ((Stack-SHL)/(SHH-SHL) * (abs(SAH - SAL))).
+
+
+% Returns the effective seat tube length ~ hypotenuse
+effective_stl(STA, Stack) ->
+    Stack / math:sin(radians(STA)).
+
+
+% Returns the effective seat tube distance, or the
+% horizontal distance from bottom bracket to the
+% effective seat tube angle at the stack height
+est_dist(STA, Stack) ->
+    math:cos(radians(STA)) * effective_stl(STA, Stack).
+
+
+% Returns the effective top tube
+ett(STA, Stack, Reach) ->
+    Reach + est_dist(STA, Stack).
