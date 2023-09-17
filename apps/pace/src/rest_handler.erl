6f222cf6f1095fd55563d74ec69d8628767ca300
aaronlelevier <aaron.lelevier@gmail.com>
2023-09-17 11:30:47 -0700

    rest_handler - example with a hello-world impl for each request type

--- apps/pace/src/rest_handler.erl
+++ apps/pace/src/rest_handler.erl
@@ -3,8 +3,13 @@
 %%
 %% https://ninenines.eu/docs/en/cowboy/2.10/manual/cowboy_rest/
 %%
-%% Example usage
-%% $ curl --header "Content-Type: application/json" localhost:8080/rest --request POST --data '{"hello":"world"}'
+%% Example usage:
+%% GET:    $ curl --header "Content-Type: application/json" localhost:8080/rest
+%% HEAD:   $ curl --header "Content-Type: application/json" -I localhost:8080/rest
+%% POST:   $ curl --header "Content-Type: application/json" localhost:8080/rest --request POST --data '{"hello":"world"}'
+%% PUT:    $ curl --header "Content-Type: application/json" localhost:8080/rest --request PUT --data '{"hello":"world"}'
+%% PATCH:  $ curl --header "Content-Type: application/json" localhost:8080/rest --request PATCH --data '{"hello":"world"}'
+%% DELETE: $ curl --header "Content-Type: application/json" localhost:8080/rest --request DELETE --data '{"hello":"world"}'
 %%
 %% @end
 %%%-------------------------------------------------------------------
@@ -17,8 +22,10 @@
     allowed_methods/2,
     content_types_provided/2,
     content_types_accepted/2,
-    handle_post/2,
-    to_json/2
+    handle_request/2,
+    to_json/2,
+    delete_resource/2,
+    delete_completed/2
 ]).
 
 init(Req, []) ->
@@ -26,7 +33,9 @@ init(Req, []) ->
 
 %% Which HTTP methods are allowed
 allowed_methods(Req, State) ->
-    {[<<"GET">>, <<"POST">>], Req, State}.
+    Allowed = [<<"GET">>, <<"HEAD">>, <<"POST">>, <<"PUT">>,
+               <<"PATCH">>, <<"DELETE">>, <<"OPTIONS">>],
+    {Allowed, Req, State}.
 
 %%% GET %%%
 
@@ -39,27 +48,65 @@ to_json(Req, State) ->
     {ok, Body, Req2} = cowboy_req:read_body(Req),
     {jsone:encode(Body), Req2, State}.
 
-%%% POST %%%
+%%% POST/PUT/PATCH %%%
 
-%% Which content types are accepted by POST/PUT requests
+%% Which content types are accepted by POST/PUT/PATCH requests
 content_types_accepted(Req, State) ->
     {
-        [{{<<"application">>, <<"json">>, []}, handle_post}],
+        [{{<<"application">>, <<"json">>, []}, handle_request}],
         Req,
         State
      }.
 
 %% Handle the POST request
-handle_post(Req, State) ->
+handle_request(Req, State) ->
     % https://ninenines.eu/docs/en/cowboy/2.10/manual/cowboy_req.read_body/
     lager:info("Req: ~p~n", [Req]),
     lager:info("State: ~p~n", [State]),
 
-    % decode POST request body
+    % decode request body
     {ok, Body, Req2} = cowboy_req:read_body(Req),
     lager:info("Body: ~p~n", [Body]),
-    lager:info("Req2: ~p~n", [Req2]),
 
-    % set response body
-    Req3 = cowboy_req:set_resp_body(jsone:encode(Body), Req),
-    {true, Req3, State}.
+    case maps:get(method, Req) of
+        <<"POST">> ->
+            handle_post(Body, Req2, State);
+        <<"PUT">> ->
+            handle_put(Body, Req2, State);
+        <<"PATCH">> ->
+            handle_patch(Body, Req2, State);
+        Other ->
+            lager:error("handle_request - Other: ~p~n", [Other]),
+            {false, Req, State}
+    end.
+
+handle_post(Body, Req, State) ->
+    lager:info("handle_post"),
+    Req2 = cowboy_req:set_resp_body(jsone:encode(Body), Req),
+    {false, Req2, State}.
+
+handle_put(Body, Req, State) ->
+    lager:info("handle_put"),
+    Req2 = cowboy_req:set_resp_body(jsone:encode(Body), Req),
+    {false, Req2, State}.
+
+handle_patch(Body, Req, State) ->
+    lager:info("handle_patch"),
+    Req2 = cowboy_req:set_resp_body(jsone:encode(Body), Req),
+    {false, Req2, State}.
+
+
+%%% DELETE %%%
+
+% Deletes the resources. Can raise an error if Result=false
+% https://ninenines.eu/docs/en/cowboy/2.10/manual/cowboy_rest/
+delete_resource(Req, State) ->
+    % decode request body
+    {ok, Body, _Req2} = cowboy_req:read_body(Req),
+    lager:info("Body: ~p~n", [Body]),
+    lager:info("delete_resource - Req: ~p State: ~p~n", [Req, State]),
+    {_Result = true, Req, State}.
+
+delete_completed(Req, State) ->
+    lager:info("delete_completed - Req: ~p State: ~p~n", [Req, State]),
+    {_Result = true, Req, State}.
